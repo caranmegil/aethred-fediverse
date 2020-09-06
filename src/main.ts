8ce6b3187fe64a8385870bc98b7e9eb3f8cdd5d2
William Moore <caranmegil@gmail.com>
2020-09-06 17:23:08 -0500

    add HomeTimeline support

--- src/main.ts
+++ src/main.ts
@@ -32,46 +32,30 @@ client
     // refreshToken = tokenData.refreshToken
 
     const activeClient = generator('pleroma', `https://${process.env.HOST}`, accessToken)
-
-    activeClient.getPublicTimeline().then( (resp: Response<Array<Entity.Status>>) => {
-      resp.data.forEach( (value: any) => {
-        console.log('-->' + encodeURI(`${process.env.PERMISSIONS_HOST}/fediverse/${value.account.acct}`))
-        request
-        .get(encodeURI(`${process.env.PERMISSIONS_HOST}/fediverse/${value.account.acct}`))
-        .then( (res) => {
-          console.log('-->' + res.body.results)
-          let permissions = res.body.results
-          if ( (permissions.indexOf("master") > -1 || permissions.indexOf("commander") >= -1) && value.content.includes(`${process.env.NAME}@${process.env.HOST}`)) {
-            request
-              .post(`${process.env.LINGUA_HOST}/`)
-              .send( {text: value.content} )
-              .then( (resl) => {
-                console.log("-->" + resl.body.response)
-              })       
-          }
-        })
-        .catch( (err) => {
-          console.log(err)
+    setInterval( () => {
+      activeClient.getPublicTimeline().then( (resp: Response<Array<Entity.Status>>) => {
+        resp.data.forEach( (value: any) => {
+          console.log('-->' + encodeURI(`${process.env.PERMISSIONS_HOST}/fediverse/${value.account.acct}`))
+          request
+            .get(encodeURI(`${process.env.PERMISSIONS_HOST}/fediverse/${value.account.acct}`))
+            .then( (res) => {
+              console.log('-->' + res.body.results)
+              let permissions = res.body.results
+              if ( (permissions.indexOf("master") > -1 || permissions.indexOf("commander") >= -1) && value.content.includes(`${process.env.NAME}@${process.env.HOST}`)) {
+                request
+                  .post(`${process.env.LINGUA_HOST}/`)
+                  .send( {text: value.content} )
+                  .then( (resl) => {
+                    console.log("-->" + resl.body.response)
+                })       
+              }
+            })
+            .catch( (err) => {
+              console.log(err)
+            })
         })
       })
-    })
- /*   const stream: WebSocketInterface = activeClient.userSocket()
-
-    stream.on('notification', (notification: Entity.Notification) => {
-      // notification.account.acct = who said it
-      // notification.status.content is what is said (HTML included)
-      // notification.type === 'mention'
-      if (notification.account.acct === 'caranmegil' && notification.type === 'mention' && notification.status !== undefined && notification.status.content !== undefined) {
-        console.log(`${notification.account.acct}: ${notification.status.content}`)
-      }
-      console.log(notification)
-    })
-   
-const activeClient2 = generator('pleroma', `https://${process.env.HOST}`, accessToken) 
-activeClient2.postStatus('greetings, all!  i\'m just merely a bot.  i promise to be good! #introduction')
-    .then( (res: Response<Entity.Status>) => { console.log(res); } )
-    .catch( err => { console.error(err); } )
-*/
+    }, 500)
   })
   .catch((err: Error) => console.error(err))
 })
